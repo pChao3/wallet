11552414f940eaef549e494645d5e3444a815f00
吕品超 <lvpinchao@xingheng.ai>
2024-06-03 18:28:53 +0800

    add feature

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { config } from './config.js';
 import './App.css';
-import WallctComp from './component/components/WalletComp.js';
-import Name from './component/components/Name.js';
-import Wrap from './Layout.js';
+
+import Header from './component/header/Header';
+import { Outlet } from 'react-router-dom';
+
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        {/* <div className="wrap">
-          <WallctComp />
-          <Name />
-        </div> */}
-        <Wrap />
+        <Header />
+        <div className="layout-content">
+          <Outlet />
+        </div>
       </QueryClientProvider>
     </WagmiProvider>
   );
