d419185dceb24dfe28d948541df7cd781e1262a5
吕品超 <lvpinchao@xingheng.ai>
2024-07-26 18:16:06 +0800

    person page scroll

--- src/pages/NFTMarket/personal.js
+++ src/pages/NFTMarket/personal.js
@@ -54,15 +54,15 @@ function Index() {
   }
 
   return (
-    <div className="container mx-auto p-6 bg-gray-300">
-      <div className="text-center mb-5 bg-sky-300">
-        <button className="flex-1 border" onClick={() => mintNFT()}>
-          mint NFT
+    <div className="container mx-auto p-6 bg-gray-300  rounded-xl">
+      <div className="text-center mb-5 bg-sky-300 rounded-md">
+        <button className="flex-1  btn" onClick={() => mintNFT()}>
+          click to mint
         </button>
         <p className="flex-1">您已拥有{data && data[0]?.result?.toString()}个该系列的NFT</p>
       </div>
       {data && data[1]?.result?.length ? (
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 gap-4 h-96 overflow-y-scroll">
           {data[1].result.map(i => {
             return <Item tokenId={i} refetchStatus={refetchStatus} key={i} />;
           })}
